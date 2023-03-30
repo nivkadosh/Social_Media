0eb826b1fcbd0f23133ab44ff69ad9af50502b69
nivkadosh <nivkadosh18192@gmail.com>
2023-03-30 17:44:57 +0300

    Resolved conflicts

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoute.js';
+import userRoutes from './routes/usersRoute.js';
+import postRoutes from './routes/postsRoute.js';
+import { register } from './controllers/authController.js';
+import { createPost } from './controllers/postsController.js';
+import { verifyToken } from './middleware/auth.js';
+import User from './models/UserModel.js';
+import Post from './models/PostModel.js';
+import { users, posts } from './data/data.js';
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
+/* ROUTES */
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    //User.insertMany(users);
+    //Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
